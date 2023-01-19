35f1bdddc125806f8572e1b4b6d3617fd1617de8
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2023-01-19 11:04:39 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	  name='PydTfinder', 
-	  version='1.1.0',  
+	  version='1.1.1',  
 	  description='::A tool to find the phase eq temperature at a given pressure with a given ΔT value::',
 	  long_description= 'Added useful features for the user.', 
 	  author='wjgoarxiv',
